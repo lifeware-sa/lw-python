2132b2a590677572cc2f87f33d27780c525d09e3
Eugenio Berretta <euberdeveloper@gmail.com>
2025-11-04 18:00:38 +0000

    test: start adding pytests

--- lw_python/tests/test_textractor.py
+++ lw_python/tests/test_textractor.py
@@ -1,18 +1,13 @@
-import unittest
-
 from pathlib import Path
+import json
 
-from lw_python.textract import textract_json_result_to_md
+from ..textract import textract_json_result_to_md
 
 test_dir = Path(__file__).parent
 assets_path = test_dir / 'assets'
 
-class TestMath(unittest.TestCase):
-    def tetest_textract_json_result_to_md(self):
-        with open(assets_path / 'textract_result.json', 'r') as textract_result:
-            result = textract_json_result_to_md(textract_result)
-        with open(assets_path / 'textract_result.md', 'r') as res:
-            res.write(result)
-
-if __name__ == "__main__":
-    unittest.main()
+def test_textract_json_result_to_md():
+    with open(assets_path / 'textract_result.json', 'r') as textract_result:
+        result = textract_json_result_to_md(json.load(textract_result))
+    with open(assets_path / 'textract_result.md', 'w') as res:
+        res.write(result)
