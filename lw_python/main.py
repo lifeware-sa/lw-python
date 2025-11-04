b30696118c530cc3a382a3ce9764cd4ae83fc1f4
Eugenio Berretta <euberdeveloper@gmail.com>
2025-11-04 16:22:19 +0000

    fix(textract): fix typo

--- lw_python/main.py
+++ lw_python/main.py
@@ -17,7 +17,8 @@ def cli():
 def textract_to_markdown(input, output):
     try:
         with open(input, 'r') as json_file:
-            markdown_text = textract.textract_json_result_to_md(json.load(json_file))
+            markdown_text = textract_json_result_to_md(json.load(json_file))
+            # markdown_text = textract.textract_json_result_to_md(json.load(json_file))
 
         with open(output, 'w') as md_file:
             md_file.write(markdown_text)
