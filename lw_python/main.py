2d067dfdaff22f7edd9470997fe0ef5a82cfbb0c
Eugenio Berretta <euberdeveloper@gmail.com>
2025-11-04 10:44:34 +0000

    fix: make it without local packages to solve pyinstaller

--- lw_python/main.py
+++ lw_python/main.py
@@ -1,6 +1,11 @@
 import click
 import json
-import textract
+# from . import textract
+
+from textractor.parsers import response_parser
+def textract_json_result_to_md(textract_json_result) -> str:
+    textracted = response_parser.parse(textract_json_result)
+    return textracted.to_markdown()
 
 @click.group()
 def cli():
