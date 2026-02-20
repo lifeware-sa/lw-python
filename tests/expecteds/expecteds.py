0ba594568fbf9f3d3df86cba5e6bd132f2d78871
Stefano Campanella <stefanocamp187@gmail.com>
2026-02-20 11:13:36 +0100

    fix: sanitize windows string in expecteds

--- tests/expecteds/expecteds.py
+++ tests/expecteds/expecteds.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 
 expecteds_path = Path(__file__).parent
 
@@ -7,10 +8,13 @@ textract_detect_result_path = expecteds_path / 'expected_textract_detect_result.
 
 def _get_raw_content(path: str):
     with open(path, 'r') as input:
-        return input.read()
+        content = input.read()
+        if os.name == "nt":
+            content = content.replace("\r\n", "\n")
+        return content
     
 def textract_analyze_result():
-    return _get_raw_content(textract_analyze_result_path)
+    return _get_raw_content(str(textract_analyze_result_path))
 def textract_detect_result():
-    return _get_raw_content(textract_detect_result_path)
+    return _get_raw_content(str(textract_detect_result_path))
 
